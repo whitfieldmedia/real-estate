8391446e902bce5b2eccc5aa24ecf5834aa24c87
spencerbackman <backmanspencer99@gmail.com>
2020-03-31 16:12:16 -0500

    styled property and property details page

--- client/src/components/Properties.js
+++ client/src/components/Properties.js
@@ -1,132 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import Map from './Mapbox';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom'
-import '../assets/css/properties.css';
+import '../assets/scss/properties.scss';
 
 export default function Properties() {
     const retsData = JSON.parse(sessionStorage.getItem('retsData'));
-    const [topProperty, setTopProperty] = useState('')
     useEffect(() => {
         window.scrollTo(0,0);
     }, [])
-    function isClicked(id) {
-        return setTopProperty(id);
-    }
     function handleClick(id) {
         sessionStorage.setItem('propertyId', id)
     }
-    function showTopProperty() {
-        return retsData.data.results.filter(res => {
-            if(res.ListingID === topProperty) {
-                return res
-            }
-            return null;
-        }).map(res => {
-            console.log(res.PhotoCount)
-            var img = `http://www.promatchcomplete.com/pictures/GNMS/Listings/c/${res.ListingID}-01.jpg?Session=531000566`
-            return (
-            <div className="property-holder"
-                key={res.ListingID}>
-                <p className="property-city">
-                    {res.City}
-                </p>
-                <h2 className="property-price">
-                    {res.ListPrice}
-                </h2>
-                <div className="property-row">
-                    <p className="property-text">
-                        <strong> {res.Bedrooms} </strong> bd
-                        <strong> {res.Baths} </strong> ba
-                        <strong> {res.LivingArea} </strong> SqFt
-                        <strong> {res.LotSizeArea} </strong> Acres
-                        <strong> {res.Parking} </strong>
-                    </p>
-                </div>
-                <div className="property-row">
-                    <img src={img} style={{ display: "block", height: 200, width: 250 }} alt={res.ListingID}/>
-                </div>
-            </div>
-        )})
-    }
     function showProperties() {
         return retsData.data.results.filter(res => {
-            if(topProperty !== '') {
-                return res.ListingID !== topProperty && res.ListAgentOfficeID !== "COLT55"
-            }
             return res.ListAgentOfficeID !== "COLT55"
         }).map(res => {
-            var img = `http://www.promatchcomplete.com/pictures/GNMS/Listings/c/${res.ListingID}-01.jpg?Session=531000566`
-            return (
-            <div className="property-holder"
-                key={res.ListingID}>
-                <p className="property-city">
-                    {res.City}
-                </p>
-                <h2 className="property-price">
-                    {res.ListPrice}
-                </h2>
-                <div className="property-row">
-                    <p className="property-text">
-                        <strong> {res.Bedrooms} </strong> bd
-                        <strong> {res.Baths} </strong> ba
-                        <strong> {res.LivingArea} </strong> SqFt
-                        <strong> {res.LotSizeArea} </strong> Acres
-                        <strong> {res.Parking} </strong>
-                    </p>
-                </div>
-                <div className="property-row">
-                    <img className="property-img" src={img} />
-                </div>
-            </div>
-        )})
+            return mapProperties(res)
+        })
     }
-    function showColtmorProperties() {
-        return retsData.data.results.filter(res => {
-            if(topProperty.length > 0) {
-                return res.ListingID !== topProperty && res.ListAgentOfficeID === "COLT55"
-            }
-            return res.ListAgentOfficeID === "COLT55"
-        }).map(res => {
-            var img = `http://www.promatchcomplete.com/pictures/GNMS/Listings/c/${res.ListingID}-01.jpg?Session=531000566`
-            var id = res.ListingID
-            return (
+    function addCommas(num) {
+        return (num + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    function mapProperties(res) {
+        var img = `http://www.promatchcomplete.com/pictures/GNMS/Listings/c/${res.ListingID}-01.jpg?Session=531000566`
+        var id = res.ListingID
+        return (
             <Link to="/property-details" className="property-holder"
                 onClick={() => handleClick(id)}
                 key={res.ListingID}>
-                <p className="property-city">
-                    {res.City}
-                </p>
-                <h2 className="property-price">
-                    {res.ListPrice}
-                </h2>
-                <div className="property-row">
-                    <p className="property-text">
-                        <strong> {res.Bedrooms} </strong> bd
-                        <strong> {res.Baths} </strong> ba
-                        <strong> {res.LivingArea} </strong> SqFt
-                        <strong> {res.LotSizeArea} </strong> Acres
-                        <strong> {res.Parking} </strong>
-                    </p>
+                <div className="property-column">
+                    <img className="property-img" src={img} alt={res.StreetNumber + " " + res.StreetName} />
                 </div>
-                <div className="property-row">
-                    <img className="property-img" src={img} />
+                <div className="property-detail-column">
+                    <p className="property-price">
+                        ${addCommas(res.ListPrice)}
+                    </p>
+                    <p className="property-city">
+                    {res.StreetNumber} {res.StreetName} {res.city}, MS
+                    </p>
+                    <div className="property-row">
+                        <p className="property-text">
+                             {res.Bedrooms} bd
+                        </p>
+                        <p className="property-text">
+                             {res.Baths} ba
+                        </p>
+                        <p className="property-text">
+                             {res.LivingArea} sqft
+                        </p>
+                        <p className="property-text">
+                             {res.LotSizeArea} Acres
+                        </p>
+                        <p className="property-text">
+                             {res.Parking}
+                        </p>                                   
+                    </div>
                 </div>
             </Link>
-        )})
+        )
+    }
+    function showColtmorProperties() {
+        return retsData.data.results.filter(res => {
+            if(res.ListAgentOfficeID === "COLT55") {
+                console.log(res)
+            }
+            console.log(res.ListAgentOfficeID)
+            if(res.ListAgentOfficeID === "COLT55") {
+                return res;
+            } else {
+                return;
+            }
+        }).map(res => {
+            return mapProperties(res)
+        })
     }
     return (
         retsData ? 
         <div className="properties-page">
-
             <div className="property-container">
-                {topProperty.length > 0 ?
-                    showTopProperty()
-                : null}
                 {showColtmorProperties()}
+            </div>
+            <div className="property-container">
                 {showProperties()}
             </div>
-            <Map isClicked={(id) => isClicked(id)} />
-
         </div>
         : 
         <div className="properties-page"></div> 
--- client/src/components/PropertyDetails.js
+++ client/src/components/PropertyDetails.js
@@ -1,77 +1,234 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
+import Gallery from 'react-grid-gallery';
+import thermometer from '../assets/icons/thermometer.svg';
+import lot from '../assets/icons/region.svg';
+import '../assets/scss/details.scss';
 
 export default function PropertyDetails () {
+    const [images, setImages] = useState([])
     const id = sessionStorage.getItem('propertyId')
     const retsData = JSON.parse(sessionStorage.getItem('retsData'));
     useEffect(() => {
         window.scrollTo(0,0);
+        setPhotos();
     }, [])
-    console.log(retsData)
-    function mapDetails () {
-        return retsData.data.results.filter(res => res.ListingID === id).map(res => {
-            const photos = res.PhotoCount;  
-            return (
-                <div className="detail-container">
-                    <p className="detail-par">
-                        {res.StreetNumber} {res.StreetName} {res.city}, MS
-                    </p>
-                    <p className="detail-price">
-                        ${res.ListPrice}
-                    </p>
-                    <p className="detail-par">
-                        {res.Bedrooms} Bedrooms
-                    </p>
-                    <p className="detail-par">
-                        Lot Size: {res.LotSizeDim}    
-                    </p>
-                    <p className="detail-par">
-                        Exterior: {res.Exterior}
-                    </p>
-                    <p className="detail-par">
-                        Features: {res.Features}
-                    </p>
-                    <p className="detail-par">
-                        Floors: {res.Floors}
-                    </p>
-                    <p className="detail-par">
-                        Heating: {res.Heating}    
-                    </p>
-                    <p className="detail-par">
-                        Parking: {res.Parking}
-                    </p>
-                    <p className="detail-par">
-                        Details: {res.PublicRemarks}
-                    </p>
-                    <p className="detail-par">
-                        Roof: {res.Roof} 
-                    </p>
-                    <p className="detail-par">
-                        Roof Age: {res.RoofAge}
-                    </p>
-                    <p className="detail-par">
-                        Stories: {res.Stories}
-                    </p>                     
-                    <p className="detail-par">
-                        Year Built: {res.YearBuilt}
-                    </p>                                                      
-                    {getImages(id, photos)}
-                </div>
-            )
+    function setPhotos() {
+        var num;
+        var alt;
+        retsData.data.results.filter(res => res.ListingID === id).map(res => {
+            num = res.PhotoCount
+            return alt = res.StreetNumber + " " + res.StreetName
         })
-    }
-    function getImages(id, num) {
-        var images = []
+        var imgArray = []
         for(var i = 0; i < num; i++) {
             var imgNum = i + 1
             if ( imgNum < 10) {
                 imgNum = "0" + imgNum
             }
-            var imgUrl = "http://www.promatchcomplete.com/pictures/GNMS/Listings/c/" + id + "-" + imgNum + ".jpg?Session=531000566"
-            images.push(imgUrl)
+            var imgUrl = "http://www.promatchcomplete.com/pictures/GNMS/Listings/B/" + id + "-" + imgNum + ".jpg?Session=531000566"
+            var img = {
+                src: imgUrl,
+                thumbnail: imgUrl,
+                thumbnailWidth: '50%',
+                thumbnailHeight: 'auto',
+                alt: alt
+            }
+            imgArray.push(img);
         }
-        return images.map(img => (
-            <img src={`${img}`} alt="Coltmor Real Estate" className="detail_photos" />
-        ))
+        return setImages(imgArray)
+    }
+    function addCommas(num) {
+        return (num + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    function mapDetails () {
+        return retsData.data.results.filter(res => res.ListingID === id).map(res => {
+            return (
+                <div className="detail-container">
+                    <div className="detail-gallery-column">
+                        <Gallery images={images} id="grid-gallery" rowHeight={225} enableImageSelection={false} />
+                    </div>     
+                    <div className="detail-column">
+                        <div className="detail-sticky-container">
+                            <p className="detail-price">
+                                ${addCommas(res.ListPrice)}
+                            </p>
+                            <h1 className="detail-address">
+                                {res.StreetNumber} {res.StreetName} {res.City}, MS
+                            </h1>
+                            <div className="detail-row">
+                                <p className="detail-text">
+                                    {res.Bedrooms} Bd
+                                </p>
+                                <p className="detail-text">
+                                    {res.Baths} Ba
+                                </p>
+                                <p className="detail-text">
+                                    {res.LivingArea} sqft
+                                </p>
+                            </div>
+                            <div className="detail-cta-row">
+                                <button className="detail-contact-button">
+                                    Contact Agent
+                                </button>
+                                <button className="detail-tour-button">
+                                    Take a Tour
+                                </button>
+                            </div>
+                        </div>
+
+                        <p className="detail-par">
+                            {res.PublicRemarks}
+                        </p>
+                        <div className="detail-facts-container">
+                            <h3 className="detail-header3">
+                                Interior Details
+                            </h3>
+                            <div className="sub-fact-holder">
+                                <div className="sub-fact-column">
+                                    <h4 className="detail-header4">
+                                        Bedrooms and Bathrooms
+                                    </h4>
+                                    <ul className="detail-list">
+                                        <li className="detail-list-item">
+                                            Rooms: {res.TotalRooms}
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Bedrooms: {res.Bedrooms}
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Bathrooms: {res.Baths}
+                                        </li>
+                                        <li className="detail-list-item">
+                                            3/4 Bathrooms: {res.BathsThreeQuarter}    
+                                        </li>
+                                        <li className="detail-list-item">
+                                            1/2 Bathrooms: {res.BathsHalf}    
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="sub-fact-column">
+                                    <h4 className="detail-header4">
+                                        Flooring
+                                    </h4>
+                                    <ul className="detail-list">
+                                        <li className="detail-list-item">
+                                            Flooring: {res.Floors}
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="sub-fact-column">
+                                    <h4 className="detail-header4">
+                                        Heating: 
+                                    </h4>
+                                    <ul className="detail-list">
+                                        <li className="detail-list-item">
+                                            Heating: {res.Heating}    
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="sub-fact-column">
+                                    <h4 className="detail-header4">
+                                        Cooling:
+                                    </h4>
+                                    <ul className="detail-list">
+                                        <li className="detail-list-item">
+                                            Cooling: {res.Cooling}
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="sub-fact-column">
+                                    <h4 className="detail-header4">
+                                        Appliances  
+                                    </h4>
+                                    <ul className="detail-list">
+                                        <li className="detail-list-item">
+                                            Appliances: {res.AppliancesThatStay}
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="sub-fact-column">
+                                    <h4 className="detail-header4">
+                                        Other Interior Features  
+                                    </h4>
+                                    <ul className="detail-list">
+                                        <li className="detail-list-item">
+                                            Livable Area: {res.LivingArea} square feet
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <h3 className="detail-header3">
+                                Property Details
+                            </h3>
+                            <div className="sub-fact-holder">
+                                <div className="sub-fact-column">
+                                    <h4 className="detail-header4">
+                                        Property
+                                    </h4>
+                                    <ul className="detail-list">
+                                        <li className="detail-list-item">
+                                            Lot Size: {res.LotSizeArea} Acres
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Stories: {res.Stories} 
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Exterior: {res.Exterior}
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Other Buildings: {res.OtherBuildings}
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <h3 className="detail-header3">
+                                Construction Details
+                            </h3>
+                            <div className="sub-fact-holder">
+                                <div className="sub-fact-column">
+                                    <h4 className="detail-header4">
+                                        Material Information
+                                    </h4>
+                                    <ul className="detail-list">
+                                        <li className="detail-list-item">
+                                            Foundation: {res.Foundation}
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Floors: {res.Floors}
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Roof: {res.Roof}
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Roof Age: {res.RoofAge} years
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Exterior: {res.Exterior}
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Windows: {res.Windows}
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="sub-fact-column">
+                                    <h4 className="detail-header4">
+                                        Condition  
+                                    </h4>
+                                    <ul className="detail-list">
+                                        <li className="detail-list-item">
+                                            Property Condition: {res.PropertyCondition}
+                                        </li>
+                                        <li className="detail-list-item">
+                                            Year Built: {res.YearBuilt}
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            )
+        })
     }
     return ( 
         <div className="detail-page">
